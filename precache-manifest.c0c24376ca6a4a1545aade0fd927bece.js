ced971eb509f868efaa6c01214e5cc9fc46ef8aa
Bishal Deb <bishaldeb656@gmail.com>
2019-11-02 16:22:15 +0530

    Updates

---
+++ precache-manifest.c0c24376ca6a4a1545aade0fd927bece.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "268ac6b8ca2171a72ddd375fb8cce6b5",
+    "url": "/demo-site/static/media/ieeemainbanner.268ac6b8.png"
+  },
+  {
+    "revision": "21d9f35dd0ae9fa3171f",
+    "url": "/demo-site/static/css/main.4961affd.chunk.css"
+  },
+  {
+    "revision": "df10083603d1b6a5ff6312018b28c820",
+    "url": "/demo-site/static/media/abhishek.df100836.png"
+  },
+  {
+    "revision": "0708455ae9ff9bef5352",
+    "url": "/demo-site/static/js/1.0708455a.chunk.js"
+  },
+  {
+    "revision": "0a2588842d0d96ee157e",
+    "url": "/demo-site/static/js/runtime~main.0a258884.js"
+  },
+  {
+    "revision": "69e895cae361266d5af6398a030e214a",
+    "url": "/demo-site/static/media/ieeelogoleft.69e895ca.jpg"
+  },
+  {
+    "revision": "21d9f35dd0ae9fa3171f",
+    "url": "/demo-site/static/js/main.21d9f35d.chunk.js"
+  },
+  {
+    "revision": "90b018899d4ffeb874759a679890ce7c",
+    "url": "/demo-site/static/media/tulogoright.90b01889.png"
+  },
+  {
+    "revision": "864be855931a6f9c2d40ebe7eb271a85",
+    "url": "/demo-site/static/media/puneet.864be855.jpg"
+  },
+  {
+    "revision": "32d0a7395d0dc5087431af3c18ddf207",
+    "url": "/demo-site/static/media/shourya.32d0a739.png"
+  },
+  {
+    "revision": "b8bf3b7b5d5be8046ccaed24197ba575",
+    "url": "/demo-site/static/media/Bhupendra.b8bf3b7b.jpg"
+  },
+  {
+    "revision": "0708455ae9ff9bef5352",
+    "url": "/demo-site/static/css/1.590d84ab.chunk.css"
+  },
+  {
+    "revision": "188f495f38505317981092823a300878",
+    "url": "/demo-site/index.html"
+  }
+];
